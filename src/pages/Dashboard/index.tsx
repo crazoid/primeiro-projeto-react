10cd136e1d8babc4c048e7656f841045e0d2a069
Fabio Palhano <crazoid@gmail.com>
2020-12-04 15:09:38 -0300

    boa

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import logoImg from '../../assets/github-logo.svg';
-import { Title, Form, Repositories, Error } from './styles';
-import { FiChevronRight } from 'react-icons/fi';
 
+import { Title, Form, Repositories, Error } from './styles';
 interface Repository {
     full_name: string;
     description: string;
@@ -17,7 +18,18 @@ interface Repository {
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState('');
     const [inputError, setInputError] = useState('');
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storageRepositories = localStorage.getItem('@GutHubExplorer:repositories');
+        if(storageRepositories){
+            return JSON.parse(storageRepositories);
+        } else {
+            return [];
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem('@GutHubExplorer:repositories', JSON.stringify(repositories));
+    }, [repositories]);
 
     async function handleAddRepository(event:FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault();
@@ -44,7 +56,7 @@ const Dashboard: React.FC = () => {
             
             <Title>Explore repositórios no GitHub.</Title>
             
-            <Form onSubmit={handleAddRepository}>
+            <Form hasError={!!inputError}  onSubmit={handleAddRepository}>
                 <input value={newRepo} onChange={(e) => setNewRepo(e.target.value)} placeholder="Digite o nome do repositório" />
                 <button type="submit">Pesquisar</button>
             </Form>
@@ -53,7 +65,7 @@ const Dashboard: React.FC = () => {
 
             <Repositories>
                 {repositories.map(repository => (
-                    <a key={repository.full_name} href="teste">
+                    <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
                         <img 
                             src={repository.owner.avatar_url}
                             alt={repository.owner.login}
@@ -64,7 +76,7 @@ const Dashboard: React.FC = () => {
                         </div>
 
                         <FiChevronRight size={20}/>
-                    </a>
+                    </Link>
                 ))}
 
             </Repositories>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { Header, RepositoryInfo, Issues } from './styles';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/github-logo.svg';
+
+interface RepositoryParams {
+    repository: string;
+}
 
 const Repository: React.FC = () => {
-    return <h1>Repository</h1>;
+    const { params } = useRouteMatch<RepositoryParams>();
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="GitHub Explorer" />
+                <Link to="/dashboard">
+                    <FiChevronsLeft size={16}/>
+                        Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="#" alt="Enatix" />
+                    <div>
+                        <strong>crazoid/conceitos-nodejs</strong>
+                        <p>descrição do repositório</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to="asdasd">
+                    <div>
+                        <strong>repository.full_name</strong>
+                        <p>repository.description</p>
+                    </div>
+                <FiChevronRight size={20}/>
+            </Link>
+            </Issues>
+        </>
+    );
  };
 
  export default Repository;
