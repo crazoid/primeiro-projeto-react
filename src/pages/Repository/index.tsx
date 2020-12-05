2c44274dd9b137149705e8a6be0aecadaaf80354
Fabio Palhano <crazoid@gmail.com>
2020-12-04 23:53:09 -0300

    foi

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,57 +1,102 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { Header, RepositoryInfo, Issues } from './styles';
 import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/github-logo.svg';
+import api from '../../services/api';
 
 interface RepositoryParams {
     repository: string;
 }
 
+interface Repository {
+    full_name: string;
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    open_issues_count: number;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
+
+interface Issue {
+    id: number;
+    title: string;
+    html_url: string;
+    user: {
+        login: string;
+    }
+}
+
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
+    const [repository, setRepository] = useState<Repository| null>(null);
+    const [issue, setIssue] = useState<Issue[]>([]);
+
+    useEffect(() => {
+        async function loadData(): Promise<void>{
+            const [repository, issues] = await Promise.all([
+                api.get(`/repos/${params.repository}`),
+                api.get(`/repos/${params.repository}/issues`)
+            ]);
+            setRepository(repository.data);
+            setIssue(issues.data);
+        }
+
+        loadData();
+
+    }, [params.repository]);
+
     return (
         <>
             <Header>
                 <img src={logoImg} alt="GitHub Explorer" />
-                <Link to="/dashboard">
+                <Link to="/">
                     <FiChevronsLeft size={16}/>
                         Voltar
                 </Link>
             </Header>
 
-            <RepositoryInfo>
+            { repository ? (
+                <RepositoryInfo>
                 <header>
-                    <img src="#" alt="Enatix" />
+                    <img src={repository.owner.avatar_url} alt={repository.owner.login} />
                     <div>
-                        <strong>crazoid/conceitos-nodejs</strong>
-                        <p>descrição do repositório</p>
+                        <strong>{repository.full_name}</strong>
+                        <p>{repository.description}</p>
                     </div>
                 </header>
                 <ul>
                     <li>
-                        <strong>1808</strong>
+                        <strong>{repository.stargazers_count}</strong>
                         <span>Stars</span>
                     </li>
                     <li>
-                        <strong>48</strong>
+                        <strong>{repository.forks_count}</strong>
                         <span>Forks</span>
                     </li>
                     <li>
-                        <strong>67</strong>
+                        <strong>{repository.open_issues_count}</strong>
                         <span>Issues</span>
                     </li>
                 </ul>
             </RepositoryInfo>
+            ) : (
+                <p>Carregando...</p>
+            )}
 
             <Issues>
-                <Link to="asdasd">
-                    <div>
-                        <strong>repository.full_name</strong>
-                        <p>repository.description</p>
-                    </div>
-                <FiChevronRight size={20}/>
-            </Link>
+                {issue.map(issue => (
+                    <a key={issue.id} href={issue.html_url}>
+                        <div>
+                            <strong>{issue.title}</strong>
+                            <p>{issue.user.login}</p>
+                        </div>
+                        <FiChevronRight size={20}/>
+                    </a>
+                ))}
             </Issues>
         </>
     );
